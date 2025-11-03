29ba31e5fbb6c14cc4c14c654f1726d005314cab
Leedoseo <okok0840@naver.com>
2025-11-03 13:41:05 +0900

    단락 평가 및 truthy_falsy 주석 수정

---
+++ javascript_advanced/short_circuit_evaluation.js
@@ -0,0 +1,49 @@
+// 단락 평가
+function returnFalse() {
+  console.log("False 함수");
+  return false;
+}
+
+function returnTrue() {
+  console.log("True 함수");
+  return true;
+}
+
+console.log(returnFalse() && returnTrue()); // False 함수, false 출력 -> 단락 평가가 작동됨
+console.log(returnTrue() && returnFalse()); // True 함수, False 함수, false 출력 -> 단락 평가 작동 X
+// -> returnTrue 만으론 첫 번째 피연산자의 값만으론 이 연산의 결과가 확정이 나지 않았기 때문!
+console.log(returnFalse() || returnTrue()); // False 함수, True 함수, true 출력
+console.log(returnTrue() || returnFalse()); // True 함수, true 출력
+
+function returnFalse1() {
+  console.log("False 함수");
+  return undefined;
+}
+
+function returnTrue1() {
+  console.log("True 함수");
+  return 10;
+}
+
+console.log(returnFalse1() && returnTrue1()); // False 함수, undefined 출력
+console.log(returnTrue1() && returnFalse1()); // True 함수, False함수, undefined 출력
+console.log(returnFalse1() || returnTrue1()); // False 함수, True 함수, 10 출력
+console.log(returnTrue1() || returnFalse1()); // True 함수, 10 출력
+
+// 단락 평가 활용 사례
+
+function printName(person) {
+  if (!person) {
+    console.log("person의 값이 없음");
+    return;
+  }
+  console.log(person.name);
+}
+
+function printName(person) {
+  const name = person && person.name;
+  console.log(name || "person의 값이 없음");
+}
+
+printName();
+printName({ name: "도서" });
