7c0b634b6b28585054427585445034b8fd8edd76
Leedoseo <okok0840@naver.com>
2025-11-12 19:39:22 +0900

    node.js ES모듈 공부

--- node.js_basic/src/index.js
+++ node.js_basic/src/index.js
@@ -5,7 +5,49 @@
 // console.log(moduleData.sub(1, 2));
 
 // 위 코드를 객체 구조분해 할당을 사용하면 이렇게 사용 가능
-const { add, sub } = require("./math");
+// const { add, sub } = require("./math");
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+// ES 모듈 이용
+// 1. 기본 import 방식
+import multiply from "./math.js";
+import { add, sub } from "./math.js";
+
+console.log("=== 기본 import ===");
+console.log(add(1, 2)); // 3
+console.log(sub(1, 2)); // -1
+console.log(multiply(3, 4)); // 12
+
+// 2. 별칭(alias) 사용
+import { add as plus, sub as minus } from "./math.js";
+
+console.log("\n=== 별칭 사용 ===");
+console.log(plus(5, 3)); // 8
+console.log(minus(5, 3)); // 2
+
+// 3. 모든 Named Export를 한 번에 가져오기
+import * as math from "./math.js";
+
+console.log("\n=== 전체 import (*) ===");
+console.log(math.add(10, 5)); // 15
+console.log(math.sub(10, 5)); // 5
+console.log(math.default(2, 6)); // 12 (default export는 .default로 접근)
+
+// 4. 새로 추가된 함수들 사용
+import { divide, power, PI } from "./math.js";
+
+console.log("\n=== 추가 함수들 ===");
+console.log(divide(10, 2)); // 5
+console.log(divide(10, 0)); // "0으로 나눌 수 없습니다"
+console.log(power(2, 3)); // 8
+console.log(PI); // 3.14159
+
+// 5. Default와 Named Export 동시에 import
+import multiply2, { add as addition, divide as div } from "./math.js";
+
+console.log("\n=== Default + Named 동시 import ===");
+console.log(multiply2(4, 5)); // 20
+console.log(addition(100, 200)); // 300
+console.log(div(20, 4)); // 5
--- node.js_basic/src/math.js
+++ node.js_basic/src/math.js
@@ -1,14 +1,47 @@
 // 덧셈 뺄셈 모듈
 
-function add(a, b) {
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// CommonJS
+// module.exports = {
+//   add /*: add,*/, // value의 값으로 사용되는 변수의 이름과 키 값이 같을 경우 생략 가능
+//   sub /*: sub,*/,
+// };
+
+// ES 모듈
+// export { add, sub }; - ES 모듈 기본
+
+// 위 덧셈 뺄셈 모듈을 이렇게 export로 붙여서 사용가능
+export function add(a, b) {
   return a + b;
 }
 
-function sub(a, b) {
+export function sub(a, b) {
   return a - b;
 }
 
-module.exports = {
-  add /*: add,*/, // value의 값으로 사용되는 변수의 이름과 키 값이 같을 경우 생략 가능
-  sub /*: sub,*/,
-};
+// math 모듈을 대표하는 단 하나의 기본값 - default
+export default function multiply(a, b) {
+  return a * b;
+}
+
+// 추가 예시 함수들
+export function divide(a, b) {
+  if (b === 0) {
+    return "0으로 나눌 수 없습니다";
+  }
+  return a / b;
+}
+
+export function power(a, b) {
+  return Math.pow(a, b);
+}
+
+// 상수 export
+export const PI = 3.14159;
