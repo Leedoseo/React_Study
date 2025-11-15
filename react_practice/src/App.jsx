ea04e5e2241c8673ff4a9fc3c4cf86d5eaa17668
Leedoseo <okok0840@naver.com>
2025-11-15 13:36:55 +0900

    React 컴포넌트

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
+// HTML 태그를 반환하는 함수를 "컴포넌트"라고 부름
 function App() {
   return (
     <>
+      <Header />
       <hi>안녕 리액트!</hi>
+      <Main />
+      <Footer />
     </>
   );
-}
+} // App(부모 컴포넌트) 아래에 Header(자식 컴포넌트)가 있음
+// 제일 위에있는 컴포넌트는 루트 컴포넌트라고 부름
 
 export default App;
---
+++ react_practice/src/components/Header.jsx
@@ -0,0 +1,17 @@
+// 컴포넌트를 생성하는 이름 첫번째 글자는 대문자로!
+function Header() {
+  return (
+    <header>
+      <hi>header</hi>
+    </header>
+  );
+}
+
+// const = Header() => { 이렇게 화살표 함수로도 가능! 편한거 사용!
+//   return (
+//     <header>
+//       <hi>header</hi>
+//     </header>
+//   );
+// }
+export default Header;
