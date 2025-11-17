1ed3de320fd42aea3c08b7df65363572e02fb4af
Leedoseo <okok0840@naver.com>
2025-11-17 15:21:51 +0900

    JSX 기본 주의사항

--- react_practice/src/components/Footer.jsx
+++ react_practice/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-    return <footer><h1>footer</h1></footer>
+    return <footer><h1>footer</h1></footer> // JS에서는 함수가 HTML을 리턴하도록 만들면 문법적인 오류로 판단함. 단, JSX파일은 오류로 판단하지 않음.
 }
 
 export default Footer;
--- react_practice/src/components/Main.jsx
+++ react_practice/src/components/Main.jsx
@@ -1,9 +1,19 @@
+// JSX 주의 사항
+// 1. 중괄호 내부에는 JS 표현식만 넣을 수 있음.
+// 2. 숫자, 문자열, 배열 값만 렌더링 됨.
+// 3. 모든 태그는 닫혀있어야 함
+// 4. 최상위 태그는 반드시 하나여야만 함
 const Main = () => {
-    return(
-        <main>
-            <h1>main</h1>
-        </main>
-    )
-}
+  const number = 10;
+
+  return (
+    <main>
+      <h1>main</h1>
+      <h1>{number}</h1>
+      <h1>{number + 10}</h1>
+      <h1>{number % 2 === 0? "짝수"  : "홀수"}</h1>
+    </main>
+  );
+};
 
 export default Main;
