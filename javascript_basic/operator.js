fc4a88a771fc681bb033c166a643bd4cff86543d
Leedoseo <okok0840@naver.com>
2025-11-28 17:03:22 +0900

    연산자 복습

--- javascript_basic/operator.js
+++ javascript_basic/operator.js
@@ -8,12 +8,13 @@ let num3 = 3 * 2;
 let num4 = 3 / 2;
 let num5 = 3 % 2;
 
-let num6 = 1 + 2 * 10;
-console.log(num6); // 곱셈이 우선 순위 ()가 있다면 ()가 우선순위
+let num6 = 1 + 2 * 10; // => 21
+// let num6 = (1 + 2) * 10; => 30
+console.log(num6); // 곱셈이 우선 순위, ()가 있다면 ()가 우선순위
 
 // 3. 복합 대입 연산자
 let num7 = 10;
-num7 += 20;
+num7 += 20; // num7에 20을 더하고 값을 출력, 풀어쓰면? -> num7 = num7 + 20 이라는 뜻
 num7 -= 20;
 num7 *= 20;
 num7 /= 20;
@@ -22,8 +23,9 @@ console.log(num7);
 
 // 4. 증감 연산자 (1씩 늘리거나 줄일 때 사용)
 let num8 = 10;
-num8++; // 후위 연산
-console.log(num8); // or (++num8) 이건 전위 연산
+console.log(++num8); // 전위 연산 -> 11 바로 1을 추가해서 11
+console.log(num8++); // 후위 연산 -> 11 1은 이 이후에 더해지기 때문에 아직은 11
+console.log(num8) // 후위 연산이 끝난 후인 12출력
 
 // 5. 논리 연산자
 let or = true || false; // 둘 중 하나만 참이면 true
@@ -56,15 +58,16 @@ let doseo1;
 let doseo2 = 10;
 let doseo3 = 20;
 
-let doseo4 = doseo1 ?? doseo2;
+let doseo4 = doseo1 ?? doseo2; // 여기서는 doseo1가 undefined, doseo2는 10이기 때문에 10이 출력
 console.log(doseo4); // 10
 
 let doseo5 = doseo1 ?? doseo3;
 console.log(doseo5); // 20
 
-let doseo6 = doseo2 ?? doseo3;
-console.log(doseo6); // 둘 다 존재하는 값이면, 앞에거를 출력 => 10
+let doseo6 = doseo2 ?? doseo3; // 둘 다 값이 존재하면 앞의 값을 출력
+console.log(doseo6); // => 10
 
+// 회원관리 시스템을 가정해서 해봄
 let userName = "도서";
 let userNickName = "이도서";
 
@@ -82,11 +85,11 @@ console.log(t1); // String 출력
 // 9. 삼항 연산자
 // -> 항을 3개 사용하는 연산자
 // -> 조건식을 이용해서 참, 거짓일 때의 값을 다르게 반환
-let var8 = 10;
+let doseo8 = 10;
 
 // 요구사항 : 변수 res에 var8의 값이 짝수 -> "짝", 홀수 -> "홀";
-let res = var8 % 2 === 0 ? "짝수" : "홀수";
+let res = doseo8 % 2 === 0 ? "짝수" : "홀수";
 // var8 % 2 === 0 -> 조건식
 // 콜론을 기준으로 왼쪽은 참, 오른쪽은 거짓
 // -> var8가 2로 나누었을 때 0으로 떨어지면 짝수, 아니라면 홀수 출력
-console.log(res);
+console.log(res); // 짝수 출력
