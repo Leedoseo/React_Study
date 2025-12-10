7cc3005167665a3e0d153f66c0e73ab88bbe8a92
Leedoseo <okok0840@naver.com>
2025-12-10 19:08:34 +0900

    State로 상태관리하기

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -1,29 +1,41 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import { useState } from "react";
 
-// HTML 태그를 반환하는 함수를 "컴포넌트"라고 부름
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
 
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>자식 요소</div>
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          -
+        </button>
+      </div>
     </>
   );
-} // App(부모 컴포넌트) 아래에 Header(자식 컴포넌트)가 있음
-// 제일 위에있는 컴포넌트는 루트 컴포넌트라고 부름
+}
 
 export default App;
